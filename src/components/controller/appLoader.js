8d7aa409b5c464a260bc980fcaaef5d75ba5d98f
Tema_sevastopol <temasebastopol@yandex.ru>
2022-12-11 22:31:59 +0300

    feat: add plugin @typescript-eslint

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -1,11 +1,15 @@
-import Loader from './loader';
-
-class AppLoader extends Loader {
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const loader_1 = __importDefault(require("./loader"));
+class AppLoader extends loader_1.default {
     constructor() {
         super('https://newsapi.org/v2/', {
             apiKey: 'b76708263a354506bf5e528ea57eb2ad', // получите свой ключ https://newsapi.org/
         });
     }
 }
-
-export default AppLoader;
+exports.default = AppLoader;
+//# sourceMappingURL=appLoader.js.map
