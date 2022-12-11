5a1ec8529d6d7c6d629b1fc8388d8df71ad56529
Tema_sevastopol <temasebastopol@yandex.ru>
2022-12-11 16:41:46 +0300

    feat: add API key

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'b76708263a354506bf5e528ea57eb2ad', // получите свой ключ https://newsapi.org/
         });
     }
 }
